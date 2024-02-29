e185dfc592414f91a595e14fe772cd466bf87828
Vadim Lemmer <lemmeronne@gmail.com>
2024-02-29 18:13:25 +0600

    ref: рефаторинг кода

--- unreal_practice_1_16.cpp
+++ unreal_practice_1_16.cpp
@@ -4,36 +4,34 @@
 const int N = 5;
 int array[N][N];
 
+int getDay()
+{
+    std::tm localTime;
+    std::time_t t = time(NULL);
+    localtime_s(&localTime, &t);
+
+    return localTime.tm_mday;
+}
+
 void fillingArray()
 {
+    int sum = 0;
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
             array[i][j] = i + j;
             std::cout << array[i][j] << " ";
+
+            sum += array[getDay() % N][j];
         }
         std::cout << "\n";
     }
     std::cout << "\n";
-}
-
-void getAmountDay()
-{
-    std::tm localTime;
-    std::time_t t = time(NULL);
-    localtime_s(&localTime, &t);
-
-    int sum = 0;
-    for (int j = 0; j < N; j++)
-    {
-        sum += array[localTime.tm_mday % N][j];
-    }
     std::cout << sum << "\n";
 }
 
 int main()
 {
     fillingArray();
-    getAmountDay();
 }
